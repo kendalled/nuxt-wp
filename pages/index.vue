16d10ebfea4b9be0d08b773c6527a65f3b20bb3c
Kendall Jackson <kendallkj00@gmail.com>
2023-12-22 15:09:09 -0500

    fix: try useHead()

--- pages/index.vue
+++ pages/index.vue
@@ -1,61 +1,32 @@
 <template>
-  <div>
-    <Head>
-      <Title>Lapel Pins & Coins: Custom Pins in Minutes.</Title>
-      <Meta
-        name="description"
-        content="An industry leader in custom made pins since 1999. Take a look at our recent orders and place a quote to get a complimentary digital render."
-      />
-    </Head>
-    <div class="relative md:pt-8 lg:pt-10">
-      <NewHero />
-      <!-- Logo Cloud -->
-      <LogoCloud />
-      <ThreeFeat />
-      <VideoSpot />
-      <TailFeat2 />
-      <SocialProof />
-      <ThreeSteps />
-      <SimpleTest />
-      <Faq2 />
-      <TailCta />
-      <TailFoot />
-    </div>
+  <div class="relative md:pt-8 lg:pt-10">
+    <NewHero />
+    <!-- Logo Cloud -->
+    <LogoCloud />
+    <ThreeFeat />
+    <VideoSpot />
+    <TailFeat2 />
+    <SocialProof />
+    <ThreeSteps />
+    <SimpleTest />
+    <Faq2 />
+    <TailCta />
+    <TailFoot />
   </div>
 </template>
 
-<script>
-import VideoSpot from '~/components/VideoSpot'
-import NewHero from '~/components/NewHero'
-import LogoCloud from '~/components/LogoCloud'
-import ThreeSteps from '~/components/ThreeSteps'
-import ThreeFeat from '~/components/ThreeFeat'
-import TailFeat2 from '~/components/TailFeat2'
-import SimpleTest from '~/components/SimpleTest'
-import TailCta from '~/components/TailCta'
-import SocialProof from '~/components/SocialProof'
-import Faq2 from '~/components/Faq2'
-import TailFoot from '~/components/TailFoot'
-export default {
-  components: {
-    VideoSpot,
-    NewHero,
-    LogoCloud,
-    ThreeFeat,
-    ThreeSteps,
-    TailFeat2,
-    SimpleTest,
-    TailCta,
-    SocialProof,
-    Faq2,
-    TailFoot
-  },
-  head () {
-    return {
-      link: [
-        { hid: 'canonical', rel: 'canonical', href: 'https://lapelpinsandcoins.com' }
-      ]
-    }
-  }
-}
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Lapel Pins & Coins: Custom Lapel Pins in Minutes.',
+  description: 'An industry leader in custom made pins since 1999. Take a look at our recent orders and place a quote to get a complimentary digital render.',
+  ogUrl: 'https://lapelpinsandcoins.com'
+})
+useHead({
+  link: [
+    {
+      rel: 'canonical',
+      href: 'https://lapelpinsandcoins.com'
+    },
+  ],
+})
 </script>
