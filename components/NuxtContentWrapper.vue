11e0dcb5adc7ec1ea3a9d4c73219666efb466b03
Kendall Jackson <kendallkj00@gmail.com>
2023-12-27 13:23:44 -0500

    fix: try JS version

--- components/NuxtContentWrapper.vue
+++ components/NuxtContentWrapper.vue
@@ -1,13 +1,12 @@
 <template>
-    <ContentQuery v-if="data" :path="$route.path" find="one" v-slot="{ data }">
-				<ContentRenderer :value="data">
-					<PinHeader :data="data" />
-					<TwoColumnContent :data="data" />
-					<PinContentImage :data="data"/>
-				</ContentRenderer>
-			</ContentQuery>
+    <ContentRenderer v-if="data" :value="data">
+			<PinHeader :data="data" />
+			<TwoColumnContent :data="data" />
+			<PinContentImage :data="data"/>
+		</ContentRenderer>
 </template>
 
 <script setup>
-    console.log('hey component')
+  const route = $this.route
+	const { data } = await useAsyncData('page-data', () => queryContent($this.route).findOne())
 </script>
