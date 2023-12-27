dcacbb5f72ff6a4927b17e71360d2e57fab3241b
Kendall Jackson <kendallkj00@gmail.com>
2023-12-27 14:03:04 -0500

    fix: remove wrapper

--- pages/pins/[...slug].vue
+++ pages/pins/[...slug].vue
@@ -2,7 +2,11 @@
 	<div>
 		<main class="bg-gray-50">
 			<!-- nuxt cms content -->
-			<NuxtContentWrapper />
+			<ContentRenderer v-if="show" :value="data">
+				<PinHeader :data="data" />
+				<TwoColumnContent :data="data" />
+				<PinContentImage :data="data"/>
+			</ContentRenderer>
 			<PinBenefits />
     		<LargeTest />
     		<TailPricing />
@@ -13,3 +17,10 @@
 		</main>
 	</div>
 </template>
+
+<script setup>
+  const route = useRoute()
+	const { data } = await useAsyncData('page-data', () => queryContent(route.path).findOne())
+	const show = (data !== null)
+	console.log(show)
+</script>
