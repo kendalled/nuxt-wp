19df7c6c149f21aabeed4ad14aca2b74d2aff093
Kendall Jackson <kendallkj00@gmail.com>
2023-12-28 11:35:33 -0500

    fix: add pin marquee to interior pages

--- components/NuxtContentWrapper.vue
+++ components/NuxtContentWrapper.vue
@@ -2,6 +2,7 @@
   <ContentQuery :path="$route.path" v-if="$route.path" find="one" v-slot="{ data }">
 		<ContentRenderer :value="data">
 			<PinHeader :data="data" />
+			<ScrollingPins/>
 			<TwoColumnContent :data="data" />
 			<PinContentImage :data="data"/>
 			</ContentRenderer>
