85f3cb5216961fb20e512c257ab8b9398f3dbc6e
Kendall Jackson <kendallkj00@gmail.com>
2024-01-17 12:45:02 -0500

    fix: emphasize leading word

--- components/ValueProp.vue
+++ components/ValueProp.vue
@@ -22,24 +22,24 @@
                 <div class="flex-shrink-0">
                   <svg class="mr-2 w-6 h-6 text-green-600" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="800px" height="800px" viewBox="0 0 36 36" aria-hidden="true" role="img" fill="currentColor" preserveAspectRatio="xMidYMid meet"><path d="M36 32a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h28a4 4 0 0 1 4 4v28z"/><path fill="#FFF" d="M29.28 6.362a2.502 2.502 0 0 0-3.458.736L14.936 23.877l-5.029-4.65a2.5 2.5 0 1 0-3.394 3.671l7.209 6.666c.48.445 1.09.665 1.696.665c.673 0 1.534-.282 2.099-1.139c.332-.506 12.5-19.27 12.5-19.27a2.5 2.5 0 0 0-.737-3.458z"/></svg>
                 </div>
-                <p class="ml-3 font-semibold text-gray-600">
-                  Same-day digital renders &amp; quote
+                <p class="ml-3 font-medium text-gray-600">
+                  <span class="font-semibold">Free</span> same-day artwork
                 </p>
               </span>
               <span class="flex items-start lg:col-span-1 lg:mt-0">
                   <div class="flex-shrink-0">
                       <svg class="mr-2 w-6 h-6 text-green-600" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="800px" height="800px" viewBox="0 0 36 36" aria-hidden="true" role="img" fill="currentColor" preserveAspectRatio="xMidYMid meet"><path d="M36 32a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h28a4 4 0 0 1 4 4v28z"/><path fill="#FFF" d="M29.28 6.362a2.502 2.502 0 0 0-3.458.736L14.936 23.877l-5.029-4.65a2.5 2.5 0 1 0-3.394 3.671l7.209 6.666c.48.445 1.09.665 1.696.665c.673 0 1.534-.282 2.099-1.139c.332-.506 12.5-19.27 12.5-19.27a2.5 2.5 0 0 0-.737-3.458z"/></svg>
                   </div>
-                <p class="ml-3 font-semibold text-gray-600">
-                  Free FedEx shipping
+                <p class="ml-3 font-medium text-gray-600">
+                  <span class="font-semibold">Free</span> FedEx shipping
                 </p>
               </span>
               <span class="flex items-start lg:col-span-1 lg:mt-0">
                 <div class="flex-shrink-0">
                   <svg class="mr-2 w-6 h-6 text-green-600" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="800px" height="800px" viewBox="0 0 36 36" aria-hidden="true" role="img" fill="currentColor" preserveAspectRatio="xMidYMid meet"><path d="M36 32a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h28a4 4 0 0 1 4 4v28z"/><path fill="#FFF" d="M29.28 6.362a2.502 2.502 0 0 0-3.458.736L14.936 23.877l-5.029-4.65a2.5 2.5 0 1 0-3.394 3.671l7.209 6.666c.48.445 1.09.665 1.696.665c.673 0 1.534-.282 2.099-1.139c.332-.506 12.5-19.27 12.5-19.27a2.5 2.5 0 0 0-.737-3.458z"/></svg>
                 </div>
-                <p class="ml-3 font-semibold text-gray-600">
-                  Unlimited revisions on all orders
+                <p class="ml-3 font-medium text-gray-600">
+                  <span class="font-semibold">Unlimited</span> revisions on all orders
                 </p>
               </span>
 
