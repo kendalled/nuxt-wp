b189bc230df337be4c85c2696dcea132efdf06fa
Kendall Jackson <kendallkj00@gmail.com>
2024-03-01 14:35:47 -0500

    fix: more redirects

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,9 @@ export default defineNuxtConfig({
   },
   routeRules: {
     '/lapel-pins': { redirect: { to: '/pins', statusCode: 301 } },
+    '/lapelpins': { redirect: { to: '/pins', statusCode: 301 } },
     '/challenge-coins': { redirect: { to: '/coins', statusCode: 301 } },
+    '/challengecoins': { redirect: { to: '/coins', statusCode: 301 } },
   },
   content: {
     // ... options
