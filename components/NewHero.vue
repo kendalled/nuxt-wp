2d1ba690bc7c35be3a8561f2794ba80cb619eaec
Kendall Jackson <kendallkj00@gmail.com>
2024-04-10 16:08:03 -0400

    fix: class names need spaces

--- components/NewHero.vue
+++ components/NewHero.vue
@@ -9,7 +9,7 @@
         </div>
         <div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
           <h1 class="text-4xl font-extrabold tracking-tight text-center sm:text-5xl lg:text-6xl">
-            <span class="blocktext-white">Get custom pins</span>
+            <span class="block text-white">Get custom pins</span>
             <span class="block text-light-blue-200">with expert guidance.</span>
           </h1>
           <p class="max-w-lg mx-auto mt-6 text-xl text-center text-light-blue-200 sm:max-w-3xl">
